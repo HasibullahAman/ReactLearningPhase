f5893f2362595638ef373731f37a60c266b7f19d
HasibullahAman <hasib.aman7@gmail.com>
2024-09-08 10:19:33 -0400

    work with jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const users = "HasibullahAman";
+  const users = [
+    {firstname: "Hasibullah Aman"},
+    {
+      firstname : "Nawidullah Aman"
+    },
+    {
+      firstname : "Mohibullah Aman"
+    },
+  ]
+
   return (
     <div className="App">
+
+      {/* { users? <h1> Hello {users} </h1> : <h1>Hello usersjan </h1>} */}
+
+      <br />
+      {
+        users.map(name => (
+          <h1> {"Hello " + name.firstname} </h1>
+        ))}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
