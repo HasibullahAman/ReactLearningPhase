485215d1d55b2044c251aedc992bc14c81fd9f84
HasibullahAman <hasib.aman7@gmail.com>
2024-09-11 10:55:18 -0400

    start stat class

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,38 +1,16 @@
-import Counter from "./component/counter";
 import { Component } from "react";
-
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>شمارنده من</h1>
-        <br />
-        <Counter count={5} firstName = "Hasibullah Aman" myArray = {[1,2,3,4,5,6]} myStat = {true} />
-        <Counter count ={10}>
-          این شمارنده من است که من خیلی دوستش دارم
-        </Counter>
-        <Counter /> 
+      <div>
+        <header>
+          <h1>شمارنده من:</h1>
+        </header>
       </div>
     );
   }
 }
 
-// class
-
-// const App = () =>{
-//     return(
-//         <div className='App'>
-//             <h1>
-//                 شمارنده من
-//             </h1>
-//             <br />
-//             <Counter />
-//             <br />
-//         </div>
-
-//     );
-// }
 
 export default App;
