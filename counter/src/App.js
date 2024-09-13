744be8fa50f475591ebb65cf7f112bee7030fc9a
HasibullahAman <hasib.aman7@gmail.com>
2024-09-13 03:27:06 -0400

    function state

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,5 +1,6 @@
 // import { Component } from "react";
 import "./App.css";
+import { useState } from "react";
 
 // Create Stat in classComponent
 
@@ -25,11 +26,36 @@ import "./App.css";
 // }
 
 const App = () => {
-  const
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Ali");
+  const [arr, setArr] =useState([1,2,3])
+
+
+  const changeCount = () =>{
+    setCount(5);
+  }
+
+  const incresNumber = () => {
+    setCount(count + 1);
+  }
+  const decresNumber = () => {
+    setCount(count - 1);
+  }
+  const resetCount = () => {
+    setCount(0);
+  }
+
   return (
     <div>
       <header>
         <h1>شمارنده من:</h1>
+        <p>{count}</p>
+        <p>{name}</p>
+        <p>{arr}</p>
+        <button onClick={changeCount}>ChangeNumber</button>
+        <button onClick={incresNumber}>+</button>
+        <button onClick={decresNumber}>-</button>
+        <button onClick={resetCount}>reset</button>
       </header>
     </div>
   );
