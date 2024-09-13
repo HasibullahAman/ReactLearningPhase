ef679b5e9cba14eb79179b835464c9bf417375bd
HasibullahAman <hasib.aman7@gmail.com>
2024-09-13 02:18:29 -0400

    create class staat

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,16 +1,38 @@
-import { Component } from "react";
+// import { Component } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>شمارنده من:</h1>
-        </header>
-      </div>
-    );
-  }
-}
+// Create Stat in classComponent
 
+// class App extends Component {
+//   state = {
+//     count : 0,
+//     name : "Hasibullah",
+//     family : "Aman"
+//   }
+//   render() {
+//     const {count,name,family} = this.state;
+//     return (
+//       <div>
+//         <header>
+//           <h1>شمارنده من:</h1>
+//         </header>
+//         <p>{count}</p>
+//         <p>{name}</p>
+//         <p>{family}</p>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const
+  return (
+    <div>
+      <header>
+        <h1>شمارنده من:</h1>
+      </header>
+    </div>
+  );
+};
 
 export default App;
